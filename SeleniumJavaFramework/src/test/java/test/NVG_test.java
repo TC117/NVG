385c141f5dc44565ae931c741667f60112857521
Master <tinb@hrcloud.com>
2019-07-26 15:20:21 +0700

    complete 4

--- SeleniumJavaFramework/src/test/java/test/NVG_test.java
+++ SeleniumJavaFramework/src/test/java/test/NVG_test.java
@@ -2,12 +2,15 @@ package test;
 
 import pages.NVGLogin;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
@@ -17,13 +20,16 @@ import org.testng.annotations.Test;
 public class NVG_test {
 	
 	WebDriver driver = null;
+	//default page
 	By btnLogin = By.xpath("//a[@id='ctl00_Header1_ctlHeaderMetaBar_ucMainLinks_hypMyAnibis']");
+	//Login page
 	By titlepri = By.xpath("//h1[@class='primary-title']");
 	By input_user = By.xpath("//input[@id='ctl00_phlContent_ctlEmailValidationBox_txtEmail']");
 	By btnSubmit = By.xpath("//a[@id='ctl00_phlContent_ctlEmailValidationBox_lbtContinue']");
 	By input_pass = By.xpath("//input[@id='ctl00_phlContent_txtPassword']");
 	By btnLoginSubmit = By.xpath("//span[contains(text(),'Se connecter')]");
 	By Logoutbtn = By.xpath("//div[@id='ctl00_Header1_ctlHeaderActionBar_ctlMemberNavigation_divLogout']//a");
+	//Search page
 	By Searchbtn = By.xpath("//span[@id='ctl00_Header1_ctlHeaderMetaBar_ucMainLinks_spSearchLongText']");
 	By Catagorybtn = By.xpath("//a[@class='category-link modal-opener']");
 	By Immobiliercat = By.xpath("//span[text()='Immobilier']");
@@ -31,8 +37,9 @@ public class NVG_test {
 	By PriceFromtext = By.xpath("//input[@id='ctl00_phlContent_Filter_ctlPriceRange_txtFrom']");
 	By PriceTotext = By.xpath("//input[@id='ctl00_phlContent_Filter_ctlPriceRange_txtTo']");
 	By ApplybtnofPrice = By.xpath("//div[text()='Prix']//following-sibling::a[@id='ctl00_phlContent_Filter_btnSearch']");
-	
-	
+	By Locationtext = By.xpath("//div[@id='ctl00_phlContent_Filter_ctlLocation_ctlLocationAutocomplete_divLocation']");
+	By DistanceDropdown = By.xpath("//select[@id='ctl00_phlContent_Filter_ctlLocation_ddlDistance']");
+	By ApplybtnofLocation = By.xpath("//div[contains(text(),'Où')]//following-sibling::a[@id='ctl00_phlContent_Filter_btnSearch']");
 	@BeforeTest
 	public void setUpTest() {
 		
@@ -119,10 +126,18 @@ public class NVG_test {
 		CatApply.click();
 		WebElement Pricefrom = driver.findElement(PriceFromtext);
 		WebElement Priceto = driver.findElement(PriceTotext);
-		WebElement Applyforprice = driver.findElement(ApplybtnofPrice);
 		Pricefrom.sendKeys("1000");
 		Priceto.sendKeys("5000");
+		WebElement Applyforprice = driver.findElement(ApplybtnofPrice);
 		Applyforprice.click();
+		WebElement Location = driver.findElement(Locationtext);
+		wait.until(ExpectedConditions.elementToBeClickable(Location));
+		Location.click();
+		Location.sendKeys("Zurich");
+		Select Distance = new Select (driver.findElement(DistanceDropdown));
+		Distance.selectByVisibleText("10 km");
+		WebElement Applyforlocation = driver.findElement(ApplybtnofLocation);
+		Applyforlocation.click();
 	}
 	
 	
